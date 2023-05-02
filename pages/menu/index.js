04c108f9fccf0aadadef1b2b0562e1ecf13c0660
amir.fahimi.aviation@gmail.com <amir.fahimi.aviation@gmail.com>
2023-05-02 23:28:03 +0430

    add .env

--- pages/menu/index.js
+++ pages/menu/index.js
@@ -9,13 +9,13 @@ function Menu({ data }) {
 export default Menu
 
 export async function getStaticProps() {
-    const res = await fetch("http://localhost:4000/data")
+    const res = await fetch(`${process.env.BASE_URL}/data`)
     const data = await res.json()
 
     return {
         props: { data },
         ////10 second
-        revalidate: 10,
+        revalidate: +process.env.REVALIDATE,
         //// 1 hour
         //revalidate: 60 * 60 ,
 
